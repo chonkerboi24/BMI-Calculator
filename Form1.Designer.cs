5deeb0001ae48400f17d5ad1c8711099753839cd
chonkerboi24 <nallanson22@gmail.com>
2024-10-29 19:31:49 +0000

    Add: calculator form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BMI_Calculator
 {
-    partial class Form1
+    partial class bmiCalculator
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.tbWeight = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(13, 13);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(61, 13);
+            this.labelHeight.TabIndex = 0;
+            this.labelHeight.Text = "Height (cm)";
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(16, 30);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(100, 20);
+            this.tbHeight.TabIndex = 1;
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Location = new System.Drawing.Point(13, 67);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(62, 13);
+            this.labelWeight.TabIndex = 2;
+            this.labelWeight.Text = "Weight (kg)";
+            // 
+            // tbWeight
+            // 
+            this.tbWeight.Location = new System.Drawing.Point(16, 83);
+            this.tbWeight.Name = "tbWeight";
+            this.tbWeight.Size = new System.Drawing.Size(100, 20);
+            this.tbWeight.TabIndex = 3;
+            // 
+            // bmiCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.tbWeight);
+            this.Controls.Add(this.labelWeight);
+            this.Controls.Add(this.tbHeight);
+            this.Controls.Add(this.labelHeight);
+            this.Name = "bmiCalculator";
+            this.Text = "BMI Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.TextBox tbWeight;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BMI_Calculator
 {
-    public partial class Form1 : Form
+    public partial class bmiCalculator : Form
     {
-        public Form1()
+        public bmiCalculator()
         {
             InitializeComponent();
         }
